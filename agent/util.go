bd27144158be3478535493859be7c7caa503e61c
James Lawrence <james@talla.com>
2017-10-02 08:34:56 -0400

    add a helper function for building local peers

--- agent/util.go
+++ agent/util.go
@@ -3,6 +3,8 @@ package agent
 import (
 	"fmt"
 	"net"
+
+	"bitbucket.org/jatone/bearded-wookie/x/systemx"
 )
 
 // RPCAddress for peer.
@@ -18,4 +20,16 @@ func SWIMAddress(p Peer) string {
 // RaftAddress for peer.
 func RaftAddress(p Peer) string {
 	return net.JoinHostPort(p.Ip, fmt.Sprint(p.RaftPort))
+}
+
+// LocalPeer build local peer.
+func LocalPeer(id string) Peer {
+	return Peer{
+		Name:     id,
+		Ip:       systemx.HostIP(systemx.HostnameOrLocalhost()).String(),
+		RPCPort:  2000,
+		SWIMPort: 2001,
+		RaftPort: 2002,
+		Status:   Peer_Ready,
+	}
 }
--- commands/spike/main.go
+++ commands/spike/main.go
@@ -1,117 +1,43 @@
 package main
 
 import (
-	"bufio"
-	"crypto/tls"
-	"fmt"
+	"context"
 	"log"
-	"net"
 	"os"
-	"path/filepath"
-	"time"
+	"sync"
 
 	"bitbucket.org/jatone/bearded-wookie"
 	"bitbucket.org/jatone/bearded-wookie/agent"
-	"bitbucket.org/jatone/bearded-wookie/x/stringsx"
+	"bitbucket.org/jatone/bearded-wookie/agentutil"
+	"bitbucket.org/jatone/bearded-wookie/ux"
+	"bitbucket.org/jatone/bearded-wookie/x/logx"
 	"bitbucket.org/jatone/bearded-wookie/x/systemx"
 
 	"github.com/alecthomas/kingpin"
-	"github.com/pkg/errors"
 )
 
 func main() {
-	var (
-		root    string
-		address = &net.TCPAddr{
-			IP:   systemx.HostIP(systemx.HostnameOrLocalhost()),
-			Port: 2000,
-		}
-	)
+	background := &sync.WaitGroup{}
+	ctx, done := context.WithCancel(context.Background())
 	app := kingpin.New("spike", "spike command line for testing functionality")
-
-	server := app.Command("server", "server").Action(server(&root, address))
-	server.Flag("bind", "bind address").TCPVar(&address)
-	server.Flag("config", "configuration file").StringVar(&root)
-	client := app.Command("client", "client").Action(client(&root, address))
-	client.Flag("config", "configuration file").StringVar(&root)
-	client.Flag("dial", "dial").TCPVar(&address)
-	// _ = app.Command("agent", "agent server").Action(agentx).Default()
-	// _ = app.Command("client", "client cli").Action(deploy)
-
+	app.Command("example1", "example 1").Action(example1(ctx, background))
 	if _, err := app.Parse(os.Args[1:]); err != nil {
-		log.Println("boom", err)
+		log.Fatalln(err)
 	}
+
+	systemx.Cleanup(ctx, done, background, os.Kill, os.Interrupt)(func() {
+		log.Println("waiting for systems to shutdown")
+	})
 }
 
-func client(root *string, address *net.TCPAddr) func(*kingpin.ParseContext) error {
+func example1(ctx context.Context, background *sync.WaitGroup) func(*kingpin.ParseContext) error {
 	return func(*kingpin.ParseContext) (err error) {
-		var (
-			config = agent.NewConfigClient()
-			cs     *tls.Config
-			conn   net.Conn
-		)
-		path := stringsx.DefaultIfBlank(*root, filepath.Join(bw.LocateDeployspace(bw.DefaultDeployspaceConfigDir), bw.DefaultEnvironmentName))
-		if err = bw.ExpandAndDecodeFile(path, &config); err != nil {
-			return errors.Wrap(err, "failed to decode config")
-		}
+		local := agent.LocalPeer(bw.MustGenerateID().String())
+		events := make(chan agent.Message, 100)
+		ux.NewTermui(ctx, background, events)
+		d := agentutil.NewBusDispatcher(events)
 
-		if cs, err = config.TLSConfig.BuildClient(); err != nil {
-			return errors.WithStack(err)
-		}
-
-		if conn, err = tls.Dial("tcp", address.String(), cs); err != nil {
-			return errors.WithStack(err)
-		}
-		defer conn.Close()
-		for t := range time.Tick(time.Second) {
-			if _, err = conn.Write([]byte(fmt.Sprintf("%s: hello world\n%s: bizz bazz\n", t, t))); err != nil {
-				return errors.WithStack(err)
-			}
-		}
-		return nil
-	}
-}
-
-func server(root *string, address *net.TCPAddr) func(*kingpin.ParseContext) error {
-	return func(*kingpin.ParseContext) (err error) {
-		var (
-			config agent.Config = agent.NewConfig()
-			cs     *tls.Config
-			l      net.Listener
-		)
-
-		path := stringsx.DefaultIfBlank(*root, bw.DefaultLocation(bw.DefaultAgentConfig, ""))
-		if err = bw.ExpandAndDecodeFile(path, &config); err != nil {
-			return err
-		}
-
-		if cs, err = config.TLSConfig.BuildServer(); err != nil {
-			return errors.WithStack(err)
-		}
-
-		if l, err = net.ListenTCP(address.Network(), address); err != nil {
-			return errors.WithStack(err)
-		}
-
-		l = tls.NewListener(l, cs)
-
-		for {
-			var (
-				conn net.Conn
-			)
-
-			if conn, err = l.Accept(); err != nil {
-				return errors.Wrap(err, "failed to accept connection")
-			}
-
-			go func(c net.Conn) {
-				s := bufio.NewScanner(c)
-				for s.Scan() {
-					log.Println("read", s.Text())
-				}
-			}(conn)
-		}
-
+		logx.MaybeLog(d.Dispatch(agentutil.PeersFoundEvent(local, 1)))
 		return nil
 	}
 }
