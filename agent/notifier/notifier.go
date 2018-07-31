a856ea19cedcde94d48e8a844edfecfe99b921fa
James Lawrence <jljatone@gmail.com>
2018-07-31 17:45:31 -0400

    more robust notifications, automatically reconnect instead of crashing

--- agent/notifier/notifier.go
+++ agent/notifier/notifier.go
@@ -1,11 +1,17 @@
 package notifier
 
 import (
+	"context"
 	"log"
 
 	"github.com/james-lawrence/bw/agent"
+	"github.com/james-lawrence/bw/agentutil"
 	"github.com/james-lawrence/bw/deployment/notifications"
 )
+
+type dialer interface {
+	Dial(p agent.Peer) (agent.Client, error)
+}
 
 // New creates a notification agent from the given configuration.
 func New(n ...notifications.Notifier) Notifier {
@@ -20,22 +26,16 @@ type Notifier struct {
 }
 
 // Start processes notifications from the client.
-func (t Notifier) Start(c agent.Client) error {
+func (t Notifier) Start(ctx context.Context, local, proxy agent.Peer, d dialer) {
 	var (
 		events = make(chan agent.Message, 5)
-		done   = make(chan struct{})
 	)
-	defer close(done)
 
-	go t.background(events, done)
+	go agentutil.WatchEvents(local, proxy, d, events)
 
-	return c.Watch(events)
-}
-
-func (t Notifier) background(events chan agent.Message, done chan struct{}) {
 	for {
 		select {
-		case <-done:
+		case <-ctx.Done():
 			return
 		case m := <-events:
 			switch event := m.GetEvent().(type) {
@@ -44,6 +44,10 @@ func (t Notifier) background(events chan agent.Message, done chan struct{}) {
 				dc := *event.DeployCommand
 				for _, n := range t.n {
 					notifyDeployCommand(n, dc)
+				}
+			case *agent.Message_Log:
+				if m.Peer != nil && m.Peer.Name == local.Name {
+					log.Println(event.Log.GetLog())
 				}
 			default:
 				// ignore other commands.
