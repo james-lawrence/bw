047a5b37242ae0bd2fb1425dbb3c8cc7a0a6aa5c
James Lawrence <jljatone@gmail.com>
2019-11-26 09:26:28 -0500

    update distributions

--- .dist/deb/build.sh
+++ .dist/deb/build.sh
@@ -6,7 +6,8 @@ export DEBUILD_DPKG_BUILDPACKAGE_OPTS="-k'${DEBFULLNAME} <${DEBEMAIL}>' -sa"
 
 ARCHIVE=../build/bearded-wookie-source-${BUILD_VERSION}.tar.gz
 DEBDIR=debian
-
+DISTRO=$1
+echo "ARGUMENTS $@"
 mkdir -p ${DEBDIR}
 mkdir -p ${DEBDIR}/source
 mkdir -p src/github.com/james-lawrence/bw
@@ -18,8 +19,8 @@ cat .templates/rules.tmpl > ${DEBDIR}/rules
 echo "9" > ${DEBDIR}/compat
 echo "3.0 (native)" > ${DEBDIR}/source/format
 
-cp ${ARCHIVE} ../bearded-wookie_${BW_VERSION}.orig.tar.gz
-tar -xf ../bearded-wookie_${BW_VERSION}.orig.tar.gz -C src/github.com/james-lawrence/bw
+cp ${ARCHIVE} ../bearded-wookie_${DISTRO}_${BW_VERSION}.orig.tar.gz
+tar -xf ../bearded-wookie_${DISTRO}_${BW_VERSION}.orig.tar.gz -C src/github.com/james-lawrence/bw
 
 debuild -S
 
