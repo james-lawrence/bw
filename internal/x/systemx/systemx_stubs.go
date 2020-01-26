4703f63352172a5dbd56aca8987d2a06efd5cb3b
James Lawrence <jljatone@gmail.com>
2020-01-26 18:07:47 -0500

    fix darwin builds

--- internal/x/systemx/systemx_stubs.go
+++ internal/x/systemx/systemx_stubs.go
@@ -4,9 +4,10 @@ package systemx
 
 import (
 	"errors"
+	"os"
 	"time"
 )
 
-func FileCreatedAt(info os.Info) (ctime time.Time, err error) {
+func FileCreatedAt(info os.FileInfo) (ctime time.Time, err error) {
 	return ctime, errors.New("unable to retrieve creation time of file outside of linux")
 }
