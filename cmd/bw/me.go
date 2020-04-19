833bc1d95a4cb831296311c23f014496f5ce1da0
James Lawrence <jljatone@gmail.com>
2020-04-19 17:01:30 -0400

    add me show to display local user credentials

--- cmd/bw/me.go
+++ cmd/bw/me.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/alecthomas/kingpin"
 	"github.com/james-lawrence/bw"
 	"github.com/james-lawrence/bw/notary"
@@ -12,7 +14,22 @@ type me struct {
 }
 
 func (t *me) configure(parent *kingpin.CmdClause) {
+	parent.Command("show", "show current credentials").Action(t.show)
 	parent.Command("init", "initialize the users credentials").Action(t.exec)
+}
+
+func (t *me) show(ctx *kingpin.ParseContext) (err error) {
+	var (
+		print string
+		pub   []byte
+	)
+	if print, pub, err = notary.AutoSignerInfo(); err != nil {
+		return err
+	}
+
+	log.Printf("fingerprint: %s\npublic key:\n%s\n", print, string(pub))
+
+	return nil
 }
 
 func (t *me) exec(ctx *kingpin.ParseContext) (err error) {
