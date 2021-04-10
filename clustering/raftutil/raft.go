2c4c390465229682675b6163a8c9a918c18278e4
James Lawrence <jljatone@gmail.com>
2021-04-10 14:57:09 -0400

    allow adjusting the passive checking for the consensus leader

--- clustering/raftutil/raft.go
+++ clustering/raftutil/raft.go
@@ -173,7 +173,7 @@ func NewProtocol(ctx context.Context, q BacklogQueueWorker, options ...ProtocolO
 		enableSingleNode: false,
 		config:           defaultRaftConfig(),
 		lastContactGrace: time.Minute,
-		PassiveCheckin:   time.Hour,
+		PassiveCheckin:   envx.Duration(time.Hour, bw.EnvAgentClusterPassiveCheckin),
 		PassiveReset: func() (Storage, raft.SnapshotStore, error) {
 			return raft.NewInmemStore(), raft.NewInmemSnapshotStore(), nil
 		},
--- internal/x/envx/envx.go
+++ internal/x/envx/envx.go
@@ -2,9 +2,13 @@
 package envx
 
 import (
+	"log"
 	"os"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/pkg/errors"
 )
 
 // Boolean retrieve a boolean flag from the environment, checks each key in order
@@ -26,6 +30,25 @@ func String(fallback string, keys ...string) string {
 		s := strings.TrimSpace(os.Getenv(k))
 		if s != "" {
 			return s
+		}
+	}
+
+	return fallback
+}
+
+// Duration retrieves a time.Duration from the environment, checks each key in order
+// first successful parse to a duration is returned.
+func Duration(fallback time.Duration, keys ...string) time.Duration {
+	for _, k := range keys {
+		s := strings.TrimSpace(os.Getenv(k))
+		if s == "" {
+			continue
+		}
+
+		if d, err := time.ParseDuration(s); err == nil {
+			return d
+		} else {
+			log.Println(errors.Wrapf(err, "unable to parse time.Duration from %s", s))
 		}
 	}
 
