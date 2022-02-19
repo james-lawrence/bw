e7ddbd2897c94c83bc7bc0f157fb4768a3717ff6
James Lawrence <jljatone@gmail.com>
2022-02-19 18:44:36 -0500

    fix tokens parsing?

--- cmd/bw/cmdopts/types.go
+++ cmd/bw/cmdopts/types.go
@@ -40,19 +40,20 @@ func ParseTCPAddr(ctx *kong.DecodeContext, target reflect.Value) (err error) {
 func ParseTCPAddrArray(ctx *kong.DecodeContext, target reflect.Value) (err error) {
 	var (
 		results []*net.TCPAddr
+		token   = ctx.Scan.Pop().String()
 	)
 
 	if ctx.Scan.Len() == 0 {
 		return nil
 	}
 
-	for _, saddr := range strings.Split(ctx.Scan.Pop().String(), "\n") {
+	for _, saddr := range strings.Split(token, "\n") {
 		var (
 			addr *net.TCPAddr
 		)
 
 		if addr, err = net.ResolveTCPAddr("tcp", saddr); err != nil {
-			return errors.Wrapf(err, "unable to resolve tcp address %s", saddr)
+			return errors.Wrapf(err, "unable to resolve tcp address %s : %s", saddr, token)
 		}
 
 		results = append(results, addr)
