a48c16f87637acd0be9e4404508d04cb9c9047cd
James Lawrence <jljatone@gmail.com>
2022-10-09 11:08:06 -0400

    properly emit vcs information

--- cmd/bw/version.go
+++ cmd/bw/version.go
@@ -1,25 +1,58 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 	"runtime/debug"
+	"strconv"
+	"time"
 
 	"github.com/james-lawrence/bw/cmd/bw/cmdopts"
+	"github.com/logrusorgru/aurora"
+	"github.com/mattn/go-isatty"
 )
 
 type cmdVersion struct{}
 
 func (t cmdVersion) Run(ctx *cmdopts.Global) (err error) {
 	var (
-		ok   bool
-		info *debug.BuildInfo
+		ok    bool
+		info  *debug.BuildInfo
+		ts    time.Time
+		id    string
+		dirty bool
 	)
 
 	if info, ok = debug.ReadBuildInfo(); ok {
-		_, err = fmt.Println(info.Main.Path, info.Main.Version)
-		return err
+		au := aurora.NewAurora(isatty.IsTerminal(os.Stdout.Fd()))
+		for _, v := range info.Settings {
+			switch v.Key {
+			case "vcs.modified":
+				if dirty, err = strconv.ParseBool(v.Value); err != nil {
+					return err
+				}
+			case "vcs.revision":
+				id = v.Value
+			case "vcs.time":
+				if ts, err = time.Parse(time.RFC3339, v.Value); err != nil {
+					return err
+				}
+			}
+		}
+
+		if _, err = fmt.Println(info.Main.Path, ts.Format("2006-01-02"), id); err != nil {
+			return err
+		}
+
+		if dirty {
+			if _, err = fmt.Println(au.Red("unsupported modified build")); err != nil {
+				return err
+			}
+		}
+
+		return nil
 	}
 
-	fmt.Println("unknown version")
-	return nil
+	return errors.New("unable to detect build information")
 }
