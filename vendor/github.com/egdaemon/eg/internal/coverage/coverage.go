5e1bb2069dc9a458c68709177403ba0e5a755b97
James Lawrence <jljatone@gmail.com>
2025-06-26 08:53:22 -0400

    update dependencies

--- vendor/github.com/egdaemon/eg/internal/coverage/coverage.go
+++
@@ -1,7 +0,0 @@
-package coverage
-
-import (
-	"github.com/egdaemon/eg/interp/events"
-)
-
-type Report = events.Coverage
--- vendor/github.com/egdaemon/wasinet/wasinet/stdlib/wasip1net/conn.wasip1.tcp.go
+++
@@ -1,9 +0,0 @@
-//go:build wasip1
-
-package wasip1net
-
-// TCPConn is an implementation of the [Conn] interface for TCP network
-// connections.
-type TCPConn struct {
-	conn
-}
