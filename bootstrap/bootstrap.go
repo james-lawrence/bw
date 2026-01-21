c71a415dc2f5226db5e20dd3ae9cbc9e6393a1a2
James Lawrence <jljatone@gmail.com>
2026-01-20 23:06:45 -0500

    fix armhf

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -59,7 +59,7 @@ func OptionBackoff(bs backoff.Strategy) func(*UntilSuccess) {
 // NewUntilSuccess continuously bootstraps until it succeeds or hits maximum attempts.
 func NewUntilSuccess(options ...option) UntilSuccess {
 	us := UntilSuccess{
-		maxAttempts: math.MaxInt64, // effectively forever.
+		maxAttempts: math.MaxInt, // effectively forever.
 		bs: backoff.New(
 			backoff.Exponential(2*time.Second),
 			backoff.Maximum(time.Minute),
